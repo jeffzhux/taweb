82b8363daa586fee9cb07fefaf669d561bfd92c2
jeffzhux <jeffzhux@gmail.com>
2023-06-29 14:29:32 +0800

    2023.6.29 taweb

--- controllers/problem_editor.js
+++ controllers/problem_editor.js
@@ -1,10 +1,10 @@
 
 const question = require('../models/question')
-const { body, validationResult } = require("express-validator");
+// const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
 
 exports.problemList = asyncHandler(async (req, res, next) =>{
     const [allQuestion] = await question.fetchAll();
-    console.log(allQuestion)
     res.render('problem_editor', { title: 'Problem Editor', questions:allQuestion });
 });
+
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 
 const problemEditorController = require('../controllers/problem_editor');
+const problemEditorPageController = require('../controllers/problem_editor_page');
 
 var router = express.Router();
 /* GET home page. */
@@ -8,8 +9,15 @@ router.get('/', function(req, res, next) {
   res.redirect('/problem_editor');
   // res.render('index', { title: 'Express' });
 });
-
+/* problem editor */
 router.get('/problem_editor', problemEditorController.problemList)
 
-// router.get('/correction')
+/* problem_editor_page */
+router.get('/problem_editor_page', problemEditorPageController.problemPage)
+router.get('/problem_editor_page/generate', problemEditorPageController.problemPage)
+router.post('/problem_editor_page/generate', problemEditorPageController.generateQuestion)
+
+/* corrections */
+router.get('/corections' )
+
 module.exports = router;
